445c06fa37c279fc45d5272dda6fb018c0f44b5e
mally <malymaly0012@gmail.com>
2024-02-26 13:44:43 +0200

    fixing food buttons and adding skin sprite

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,43 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
+using TMPro;
+using UnityEngine.UI;
+
+
 
 public class Food : MonoBehaviour
-
 {
-    int value = 0;
-
+    public int goldenEggsCost = 10;
+    public Button foodButton;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        foodButton.onClick.AddListener(PurchaseFood);
     }
 
-    // Update is called once per frame
-    void Update()
+    void PurchaseFood()
     {
-        if (gameObject.activeSelf)
+        // Check if the player has enough golden eggs to afford the food item
+        if (CanAfford())
         {
+            PlayerPrefs.SetInt("GoldenEggs", PlayerPrefs.GetInt("GoldenEggs") - goldenEggsCost);
+
             PlayerPrefs.SetInt("XP", PlayerPrefs.GetInt("XP") + XPValue());
-            GameManager.AddXP("Zero"); //to update the level
+
+            GameManager.AddXP("Zero");
 
             gameObject.SetActive(false);
         }
-
+        else
+        {
+            Debug.Log("Not enough golden eggs to purchase this food!");
+        }
     }
 
     int XPValue()
     {
-        if (gameObject.name == "strawberrycheese")
+        if (gameObject.name == "food")
         {
             return 1;
-        }//...
-
-
-
+        }
+       
         return 0;
     }
 
+    // Check if the player has enough golden eggs
+    bool CanAfford()
+    {
+        return PlayerPrefs.GetInt("GoldenEggs") >= goldenEggsCost;
+    }
 }
